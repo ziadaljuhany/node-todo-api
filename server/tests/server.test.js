5348904504cea8931fcfe60a253603b0c987d07d
Ziad Al Juhany <45928126+ziadaljuhany@users.noreply.github.com>
2019-03-07 09:29:02 +0700

    Update server.test.js

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,7 +5,7 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 beforeEach((done) => {
-	Todo.deleteOne({}).then(() => done());
+	Todo.deleteMany({}).then(() => done());
 });
 
 describe('POST /todos', () => {
