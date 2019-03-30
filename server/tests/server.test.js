e624f8cc19a24e2983e964dac0548e8e797d4384
ziadaljuhany <ziadaljuhany@gmail.com>
2019-03-30 10:21:51 +0700

    Update config to use json file

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -218,7 +218,7 @@ describe('PATCH /todos/:id', () => {
 			.expect((res) => {
 				expect(res.body.todo.text).toBe(text);
 				expect(res.body.todo.completed).toBe(false);
-				expect(typeof res.body.todo.completedAt).toBeTruthy();
+				expect(res.body.todo.completedAt).toBeFalsy();
 			})
 			.end(done);
 	});
@@ -316,7 +316,7 @@ describe('POST /users/login', () => {
 				}
 
 				User.findById(users[1]._id).then((user) => {
-					expect(user.tokens[1]).toMatchObject({
+					expect(user.toObject().tokens[1]).toMatchObject({
 						access: 'auth',
 						token: res.headers['x-auth']
 					});
